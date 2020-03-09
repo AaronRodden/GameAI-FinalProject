f9532ad95184b9bcce9f54a95783427262ec58ca
Aaron Rodden <aaron.rodden21@gmail.com>
2020-03-09 13:11:26 -0700

    More directory moving

--- The_Game/classifiers/prediction_driver.py
+++ The_Game/classifiers/prediction_driver.py
@@ -1,6 +1,9 @@
 from keras.models import load_model
 import numpy as np
-
+import sys 
+import os
+#TODO: #Is there a better way to do this??
+sys.path.append(os.path.abspath("..")) #Some python directory stuff we can do to access files
 from lib import video_processor
 
 f = load_model('CNNmodel.h5')
