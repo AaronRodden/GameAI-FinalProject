cb615cc0f41ba59db4a121746bb6fd353653b4c3
Jeffrey <wujeffrey567@gmail.com>
2020-03-10 15:52:55 -0700

    updated layers in CNN

--- The_Game/classifiers/classifier.py
+++ The_Game/classifiers/classifier.py
@@ -17,21 +17,22 @@ data_test = pd.read_csv("../dataset/sign_mnist_test.csv")
 data_train = pd.read_csv("../dataset/sign_mnist_train.csv")
 
 
-labels = data_train['label']
-data_train.drop('label', axis=1, inplace=True)
-labels_test = data_test['label']
-data_test.drop('label', axis=1, inplace=True)
 
+y_train = data_train['label'].values
+y_test = data_test['label'].values
 
-X_train = np.array(data_train.iloc[:,:])
+X_train = data_train.drop(['label'],axis=1)
+X_test = data_test.drop(['label'], axis=1)
+
+X_train = np.array(X_train.iloc[:,:])
 X_train = np.array([np.reshape(i, (28,28)) for i in X_train])
-X_test = np.array(data_test.iloc[:,:])
+
+X_test = np.array(X_test.iloc[:,:])
 X_test = np.array([np.reshape(i, (28,28)) for i in X_test])
 
 num_classes = 26
-
-y_train = np.array(labels).reshape(-1)
-y_test = np.array(labels_test).reshape(-1)
+y_train = np.array(y_train).reshape(-1)
+y_test = np.array(y_test).reshape(-1)
 
 y_train = np.eye(num_classes)[y_train]
 y_test = np.eye(num_classes)[y_test]
@@ -41,20 +42,26 @@ X_test = X_test.reshape((7172, 28, 28, 1))
 
 
 # CNN ---------------------------------------------------------------------
-classifier = Sequential()
-classifier.add(Conv2D(filters=8, kernel_size=(3,3),strides=(1,1),padding='same',input_shape=(28,28,1),activation='relu', data_format='channels_last'))
-classifier.add(MaxPooling2D(pool_size=(2,2)))
-classifier.add(Conv2D(filters=16, kernel_size=(3,3),strides=(1,1),padding='same',activation='relu'))
-classifier.add(Dropout(0.5))
-classifier.add(MaxPooling2D(pool_size=(4,4)))
-classifier.add(Dense(128, activation='relu'))
-classifier.add(Flatten())
-classifier.add(Dense(26, activation='softmax'))
+model = Sequential()
+model.add(Conv2D(64, kernel_size=(3,3), activation = 'relu', input_shape=(28, 28 ,1) ))
+model.add(MaxPooling2D(pool_size = (2, 2)))
 
-classifier.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
-classifier.fit(X_train, y_train, epochs=50, batch_size=100)
+model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
 
-accuracy = classifier.evaluate(x=X_test,y=y_test,batch_size=32)
+model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(0.20))
+
+model.add(Dense(num_classes, activation = 'softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=50, batch_size=100)
+
+accuracy = model.evaluate(x=X_test,y=y_test,batch_size=32)
 print("Accuracy: ",accuracy[1])
 
-classifier.save('CNNmodel.h5')
+model.save('CNNmodel.h5')
